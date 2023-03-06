b9ea1ea19d6baae4f0d66f37b80c7b5e69748ee2
melektron <matteo@elektron.work>
2023-03-06 17:16:11 +0000

    made create play songs

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <kipr/time/time.h>
 #include <iostream>
 #include <iomanip>
+#include "pitches.h"
 
 using namespace kipr::motor;
 using namespace kipr::servo;
@@ -17,6 +18,274 @@ using namespace kipr::analog;
 
 #define ANGLE_MUlT 1 / 1.3
 
+uint16_t treble_melody[64] = {
+    NR_NOTE_E5,
+    NR_NOTE_E3,
+    NR_NOTE_B4,
+    NR_NOTE_C5,
+    NR_NOTE_D5,
+    NR_NOTE_E5,
+    NR_NOTE_D5,
+    NR_NOTE_C5,
+    NR_NOTE_B4,
+    NR_NOTE_A4,
+    NR_NOTE_A3,
+    NR_NOTE_A4,
+    NR_NOTE_C5,
+    NR_NOTE_E5,
+    NR_NOTE_A3,
+    NR_NOTE_D5,
+    NR_NOTE_C5,
+    NR_NOTE_B4,
+    NR_NOTE_E4,
+    NR_NOTE_G4,
+    NR_NOTE_C5,
+    NR_NOTE_D5,
+    NR_NOTE_E3,
+    NR_NOTE_E5,
+    NR_NOTE_E3,
+    NR_NOTE_C5,
+    NR_NOTE_A3,
+    NR_NOTE_A4,
+    NR_NOTE_A3,
+    NR_NOTE_A4,
+    NR_NOTE_A3,
+    NR_NOTE_B2,
+    NR_NOTE_C3,
+    NR_NOTE_D3,
+    NR_NOTE_D5,
+    NR_NOTE_F5,
+    NR_NOTE_A5,
+    NR_NOTE_C5,
+    NR_NOTE_C5,
+    NR_NOTE_G5,
+    NR_NOTE_F5,
+    NR_NOTE_E5,
+    NR_NOTE_C3,
+    0,
+    NR_NOTE_C5,
+    NR_NOTE_E5,
+    NR_NOTE_A4,
+    NR_NOTE_G4,
+    NR_NOTE_D5,
+    NR_NOTE_C5,
+    NR_NOTE_B4,
+    NR_NOTE_E4,
+    NR_NOTE_B4,
+    NR_NOTE_C5,
+    NR_NOTE_D5,
+    NR_NOTE_G4,
+    NR_NOTE_E5,
+    NR_NOTE_G4,
+    NR_NOTE_C5,
+    NR_NOTE_E4,
+    NR_NOTE_A4,
+    NR_NOTE_E3,
+    NR_NOTE_A4,
+    0,
+};
+
+uint16_t bass_melody[64] = {
+    NR_NOTE_E4,
+    NR_NOTE_E3,
+    NR_NOTE_A2,
+    NR_NOTE_E3,
+    NR_NOTE_C4,
+    NR_NOTE_E3,
+    NR_NOTE_A2,
+    NR_NOTE_E3,
+    NR_NOTE_D4,
+    NR_NOTE_E3,
+    NR_NOTE_GS2,
+    NR_NOTE_E3,
+    NR_NOTE_B3,
+    NR_NOTE_E3,
+    NR_NOTE_GS2,
+    NR_NOTE_E3,
+    NR_NOTE_C4,
+    NR_NOTE_E3,
+    NR_NOTE_A2,
+    NR_NOTE_E3,
+    NR_NOTE_A3,
+    NR_NOTE_E3,
+    NR_NOTE_A2,
+    NR_NOTE_E3,
+    NR_NOTE_GS3,
+    NR_NOTE_E3,
+    NR_NOTE_GS2,
+    NR_NOTE_E3,
+    NR_NOTE_B3,
+    NR_NOTE_E3,
+    NR_NOTE_GS2,
+    NR_NOTE_E3,
+    NR_NOTE_E4,
+    NR_NOTE_E3,
+    NR_NOTE_A2,
+    NR_NOTE_E3,
+    NR_NOTE_C4,
+    NR_NOTE_E3,
+    NR_NOTE_A2,
+    NR_NOTE_E3,
+    NR_NOTE_D4,
+    NR_NOTE_E3,
+    NR_NOTE_GS2,
+    NR_NOTE_E3,
+    NR_NOTE_B3,
+    NR_NOTE_E3,
+    NR_NOTE_GS2,
+    NR_NOTE_E3,
+    NR_NOTE_C4,
+    NR_NOTE_E3,
+    NR_NOTE_E4,
+    NR_NOTE_E3,
+    NR_NOTE_A4,
+    NR_NOTE_E3,
+    NR_NOTE_A2,
+    NR_NOTE_E3,
+    NR_NOTE_GS4,
+    NR_NOTE_E3,
+    NR_NOTE_GS2,
+    NR_NOTE_E3,
+    NR_NOTE_GS2,
+    NR_NOTE_E3,
+    NR_NOTE_GS2,
+    NR_NOTE_E3,
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc
+uint8_t treble_durations[64] = {
+    8,
+    8,
+    8,
+    8,
+    8,
+    16,
+    16,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    4,
+    8,
+    8,
+    16,
+    16,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    16,
+    16,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    4,
+    4,
+};
+
+uint8_t bass_durations[64] = {
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+    8,
+};
 
 void grab_cube(Analog &dist_sens, Servo &grab_serv, Arm &my_arm)
 {
@@ -48,10 +317,69 @@ void grab_cube(Analog &dist_sens, Servo &grab_serv, Arm &my_arm)
 
 int main()
 {
+
     if (!create_connect())
     {
+        float speed = 1.5;
+        uint8_t tr1[32]; float t1 = 0;
+        for (int i = 0; i < 16; i++)
+        {
+            tr1[i * 2] = treble_melody[i];
+            int t = (64 * speed) / treble_durations[i];
+            tr1[(i * 2) + 1] = t;
+            t1 += (float)t / 64;
+        }
+        uint8_t tr2[32]; float t2 = 0;
+        for (int i = 0; i < 16; i++)
+        {
+            tr2[i * 2] = treble_melody[i+16];
+            int t = (64 * speed) / treble_durations[i+16];
+            tr2[(i * 2) + 1] = t;
+            t2 += (float)t / 64;
+        }
+        uint8_t tr3[32]; float t3 = 0;
+        for (int i = 0; i < 16; i++)
+        {
+            tr3[i * 2] = treble_melody[i+32];
+            int t = (64 * speed) / treble_durations[i+32];
+            tr3[(i * 2) + 1] = t;
+            t3 += (float)t / 64;
+        }
+        uint8_t tr4[32]; float t4 = 0;
+        for (int i = 0; i < 16; i++)
+        {
+            tr4[i * 2] = treble_melody[i+48];
+            int t = (64 * speed) / treble_durations[i+48];
+            tr4[(i * 2) + 1] = t;
+            t4 += (float)t / 64;
+        }
+        uint8_t bass_assembled[128];
+        for (int i = 0; i < 16; i++)
+        {
+            bass_assembled[i * 2] = bass_melody[i];
+            bass_assembled[(i * 2) + 1] = (64 * speed) / bass_durations[i];
+        }
+        create_spin_CW(100);
+        create_load_song(tr1, 16, 0);
+        create_load_song(tr2, 16, 1);
+        create_load_song(tr3, 16, 2);
+        create_load_song(tr4, 16, 3);
+        create_play_song(0);
+        msleep(1000);
+        while (get_create_song_playing()) msleep(1);
+        create_play_song(1);
+        msleep(1000);
+        while (get_create_song_playing()) msleep(1);
+        create_play_song(2);
+        msleep(1000);
+        while (get_create_song_playing()) msleep(1);
+        create_play_song(3);
+        msleep(1000);
+        while (get_create_song_playing()) msleep(1);
+        create_disconnect();
+        return 0;
         set_create_distance(0);
-        create_drive_direct(-200, -200);
+        create_drive_direct(200, 200);
         int ms = 0;
         while ( ms < 2000)
         {
